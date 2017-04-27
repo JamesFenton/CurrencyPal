79404d3d43bcda43f4dd4d01468c259725235fd0
James Fenton <jamesfenton777@gmail.com>
2017-04-27 22:03:09 +0400

    dotnet publish after aurelia-cli

--- Build.ps1
+++ Build.ps1
@@ -1,10 +1,10 @@
 dotnet restore
 
-dotnet publish "$PSScriptRoot\src\CurrencyPal" -o "$PSScriptRoot\publish"
-
 Push-Location "$PSScriptRoot\src\CurrencyPal"
 yarn
 npm run build
 Pop-Location
+
+dotnet publish "$PSScriptRoot\src\CurrencyPal" -o "$PSScriptRoot\publish"
 
 7z a Rates.zip .\publish\**
