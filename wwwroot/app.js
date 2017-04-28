8434cafec62263d2d3f5202b2984e1abcf2dd0da
James Fenton <jamesfenton777@gmail.com>
2017-04-28 15:18:15 +0400

    Round rates to 2 decimal places

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,16 +1,24 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    rates: []
-  },
-  methods: {
-      getRates: function() {
-          fetch("api/rates")
-            .then(res => res.json())
-            .then(rates => this.rates = rates);
-      }
-  },
-  created: function() {
-    this.getRates();
-  }
+    el: '#app',
+    data: {
+        rates: []
+    },
+    methods: {
+        getRates: function() {
+            fetch("api/rates")
+                .then(res => res.json())
+                .then(rates => this.rates = rates);
+        }
+    },
+    filters: {
+        round: function (value) {
+            if (!value) 
+                return '';
+            var rounded = Math.round(value * 100) / 100;
+            return rounded;
+        }
+    },
+    created: function() {
+        this.getRates();
+    }
 })
