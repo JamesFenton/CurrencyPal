8f2bdafc45ce921ec68215fc2ceb9257eccec511
James Fenton <jamesfenton777@gmail.com>
2017-05-03 20:43:23 +0200

    Show update time

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,13 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        rates: []
+        rates: [],
+        updateTime: null
     },
     methods: {
         getRates: function() {
             fetch("api/rates")
                 .then(res => res.json())
-                .then(rates => this.rates = rates);
+                .then(dto => {
+                    this.rates = dto.rates;
+                    var updated = new Date(dto.updateTime);
+                    this.updateTime = "Last updated at " + updated.toLocaleTimeString();
+                });
         }
     },
     filters: {
