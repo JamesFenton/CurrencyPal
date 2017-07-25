dcbd35797a24a064390d2e7936ca9783e84beb7d
James Fenton <jamesfenton777@gmail.com>
2017-07-25 20:14:32 +0200

    Updated build process to include Rates.Fetcher

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,31 @@
 $version = $env:APPVEYOR_BUILD_VERSION
-$webPublishDirectory = "$PSScriptRoot\publish\web"
 
+function Test-ExitCode($exitCode) {
+	if ($exitCode -ne 0) {
+		exit $exitCode
+	}
+}
+
+# restore
 dotnet restore
+Test-ExitCode $lastExitCode
 
+# build
+dotnet build
+Test-ExitCode $lastExitCode
+
+# publish web
+$webPublishDirectory = "$PSScriptRoot\publish\web"
+Push-Location "$PSScriptRoot\src\Rates.Web"
 dotnet publish -o $webPublishDirectory -c Release
+Test-ExitCode $lastExitCode
 $version | Out-File "$webPublishDirectory\version.txt"
+Pop-Location
 
+# zip web
 7z a "Rates.Web.$version.zip" .\publish\web\**
+Test-ExitCode $lastExitCode
+
+# zip fetcher
+7z a "Rates.Fetcher.$version.zip" .\src\Rates.Fetcher\bin\Debug\net461\win7-x86\**
+Test-ExitCode $lastExitCode
