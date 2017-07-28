dcd99ed9010b40b1e84d0d19e88b0c2098009693
James Fenton <jamesfenton777@gmail.com>
2017-07-28 12:44:29 +0200

    Add deploy.ps1 to start the website after deploying

---
+++ src/Rates.Web/deploy.ps1
@@ -0,0 +1,4 @@
+﻿# This is required because the AppVeyor deployment agent fails to deploy the website:
+# Web Deploy cannot modify the file 'Rates.Web.exe' on the destination because it is locked by an external process
+
+Start-Website Rates
