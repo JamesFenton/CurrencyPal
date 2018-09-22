edd53417cfaac873e45a43f97698c0bcea0f4656
James Fenton <jamesfenton777@gmail.com>
2018-09-22 19:20:44 +0200

    Disable 7z

--- Build.ps1
+++ Build.ps1
@@ -25,9 +25,9 @@ Test-ExitCode $lastExitCode
 $version | Out-File "$webPublishDirectory\version.txt"
 
 # zip web
-7z a "Rates.Web.$version.zip" .\publish\web\**
-Test-ExitCode $lastExitCode
+#7z a "Rates.Web.$version.zip" .\publish\web\**
+#Test-ExitCode $lastExitCode
 
 # zip fetcher
-7z a "Rates.Fetcher.$version.zip" .\src\Rates.Fetcher\bin\Release\net461\**
-Test-ExitCode $lastExitCode
+#7z a "Rates.Fetcher.$version.zip" .\src\Rates.Fetcher\bin\Release\net461\**
+#Test-ExitCode $lastExitCode
