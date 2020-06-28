7521628c79fd484898d869c9fb18080766dbc67b
James Fenton <jamesfenton777@gmail.com>
2020-06-28 16:51:31 +0200

    Fixed display for 0

--- src/Rates.Web/components/RatesValue.jsx
+++ src/Rates.Web/components/RatesValue.jsx
@@ -6,13 +6,24 @@ class RatesValue extends Component {
         const {rate} = this.props;
 
         return (
-            <td style={rate > 0 ? {color: 'MediumSeaGreen'} : {color: 'Tomato'}}>
+            <td style={this.getStyle(rate)}>
                 {this.roundedPercentage(rate)}
             </td>
         )
     }
 
+    getStyle(rate) {
+        if (rate === 0)
+            return null;
+        if (rate > 0)
+            return {color: 'MediumSeaGreen'};
+        else if (rate < 0)
+            return {color: 'Tomato'};
+    }
+
     roundedPercentage(value) {
+        if (value === 0)
+            return '0.00%';
         if (!value)
             return '';
         return value.toFixed(2) + '%';
